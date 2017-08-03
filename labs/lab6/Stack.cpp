166cbced40c4179f1c9eb6f45ed79c1fa7cce599
Mark Fontaine <fontainm@flip3.engr.oregonstate.edu>
2017-08-03 14:03:15 -0700

    lab6 work

--- labs/lab6/Stack.cpp
+++ labs/lab6/Stack.cpp
@@ -4,7 +4,7 @@ Stack::Stack(){
   arraySize = 0;
 }
 
-int Stack::inputNumbers(){
+char Stack::inputNumbers(){
   cout << "input an element of your equation" << endl;
   cin >> pushChar;
   if (cin.fail()){
@@ -20,51 +20,13 @@ int Stack::setArraySize(){
 }
 
 void Stack::push(){
-  setArraySize();
-  numArray = new char[arraySize];
-  for (int i = 0; i < arraySize; i++){
-    numArray[i] = inputNumbers();
-  }
-  convertToInt();
-  cout << "1st number: " << popNumOne << endl;
-  cout << "2nd number: " << popNumTwo << endl;
+  Node* newNode = new Node;
+  newNode->element = inputNumbers();
 }
 
-void Stack::setPopNumOne(){
-  popNumOne = tempChar - '0';
-  numCount++;
-  cout << "numCount = " << numCount << endl;
-}
+void Stack::findNumbers(){
 
-void Stack::setPopNumTwo(){
-  popNumTwo = tempChar - '0';
-  numCount++;
-  cout << "numCount = " << numCount << endl;
 }
-
 
 void Stack::convertToInt(){
-  int numCount = 0;
-  char tempChar;
-  for (int i = arraySize; i >= 0; i--){
-    tempChar = numArray[i];
-    if (numArray[i] >= '0' && numArray[i] <= '9' && numCount == 0){
-      setPopNumOne();
-    }
-    else if (numArray[i] >= '0' && numArray[i] <= '9' && numCount == 1){
-      setPopNumTwo();
-    }
-    if (numCount == 2){
-      for (int i = arraySize; i >= 0; i--){
-        tempChar = numArray[i];
-      }
-      switch (tempChar){
-        case '+':
-          popNum = popNumOne + popNumTwo;
-          cout << "sum of " << popNumOne << "+" << popNumTwo <<  " is " << popNum << endl;
-      }
-    }
-
-  }
-
 }
--- labs/lab6/Stack.h
+++ labs/lab6/Stack.h
@@ -3,25 +3,27 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include "Structs.h"
 
 using namespace std;
 
 class Stack {
   private:
-    char* numArray;
+    char* charArray;
     int arraySize;
     char pushChar;
     int popNumOne;
     int popNumTwo;
-    int popNum;
     int numCount;
+    char tempChar;
 
   public:
     Stack();
-    int inputNumbers();
+    char inputNumbers();
     void inputOperand();
     int setArraySize();
     void convertToInt();
+    void findNumbers();
     void push();
     void pop();
 };
